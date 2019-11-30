56845dec8674f376ef35ff1f793432514be348ee
Denis <potekhin@umgoroda.ru>
2019-11-30 11:56:53 +0100

    Add pagination. Modify URL rules

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -6,6 +6,7 @@ namespace app\controllers;
 use app\models\Category;
 use app\models\Product;
 use Yii;
+use yii\data\Pagination;
 
 class CategoryController extends AppController
 {
@@ -17,9 +18,21 @@ class CategoryController extends AppController
 
     public function actionView() {
         $id = Yii::$app->request->get('id');
-        $products = Product::find()->where(['category_id' => $id])->all();
+
         $category = Category::findOne($id);
+        if(empty($category))
+            throw new \yii\web\HttpException(404, 'Выбранной категории не существует');
+
+
+
+        $query = Product::find()->where(['category_id' => $id]);
+        $pages = new Pagination(['totalCount' => $query->count(),
+            'pageSize' => 2,
+            'forcePageParam' => FALSE,
+            'pageSizeParam' => FALSE]);
+        $products = $query->offset($pages->offset)->limit($pages->limit)->all();
+
         $this->setMeta('E-Shop | ' . $category->name, $category->keywords, $category->description);
-        return $this->render('view', compact('products', 'category'));
+        return $this->render('view', compact('products', 'pages', 'category'));
     }
 }
--- views/category/view.php
+++ views/category/view.php
@@ -4,6 +4,7 @@
 
 use yii\web\View;
 use yii\helpers\Html;
+use yii\widgets\LinkPager;
 ?>
 
 <section id="advertisement">
@@ -86,17 +87,18 @@ use yii\helpers\Html;
                                 <div class="clearfix"></div>
                             <?php endif; ?>
                         <?php endforeach; ?>
+                            <div class="clearfix"></div>
+                            <?php
+                                echo LinkPager::widget([
+                                'pagination' => $pages,
+                                ]);
+                            ?>
                         <?php else: ?>
                             <h2>В выбранной категории нет товаров</h2>
                         <?php endif; ?>
-                        <div class="clearfix"></div>
 
-						<ul class="pagination">
-							<li class="active"><a href="">1</a></li>
-							<li><a href="">2</a></li>
-							<li><a href="">3</a></li>
-							<li><a href="">&raquo;</a></li>
-						</ul>
+
+
 					</div><!--features_items-->
 				</div>
 			</div>
