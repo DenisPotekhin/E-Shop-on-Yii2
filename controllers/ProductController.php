4a600c5928c8b3c1eafeebefde48af7b65b8ec7c
Denis <potekhin@umgoroda.ru>
2019-12-01 12:24:46 +0100

    Product Card, ver 2.0

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -16,9 +16,10 @@ class ProductController extends AppController
     public function actionView() {
         $id = Yii::$app->request->get('id');
         $product = Product::findOne($id);
+        $hits = Product::find()->where(['hit' => '1'])->limit(6)->all();
         //Вариант для жадной загрузки
         //$product = Product::find()->with('category')->where(['id' => $id])->limit('1')->one();
-
-        return $this->render('view', compact('product'));
+        $this->setMeta('E-Shop | ' . $product->name, $product->keywords, $product->description);
+        return $this->render('view', compact('product', 'hits'));
     }
 }
